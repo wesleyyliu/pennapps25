962f8df8e90060d232c17ed5c3df963c04bb240d
Divak2004 <fu_david@outlook.com>
2024-09-21 20:22:13 -0400

    Imported some file stuff

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from typing import Union, Annotated
+
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/bugs/")
+def get_bugs():
+    return "Sussy bugs"
+
+@app.post("/uploadfile/")
+async def create_upload_file(
+    file: Annotated[UploadFile, File(description="A file read as UploadFile")],
+):
+    return {"filename": file.filename}
